959237f88ac3cc9d985d956bd9b36b8546a72521
Raymond <101374892+raymonable@users.noreply.github.com>
2023-07-20 14:29:44 -0400

    remove changes

--- dlls/kernel32/module.c
+++ dlls/kernel32/module.c
@@ -342,14 +342,7 @@ static inline FARPROC get_proc_address_wrapper( HMODULE module, LPCSTR function 
 
 #endif /* __x86_64__ */
 
-// We also need to detect if "module" is "ntdll.dll" but that can probably done easily
-LPCSTR wine_get_version_name = "wine_get_version";
-LPCSTR bullshit_name = "wine_fuck_yourself";
 FARPROC WINAPI GetProcAddress( HMODULE hModule, LPCSTR function )
 {
-    if (!strcmp(function, wine_get_version_name)) {
-        return get_proc_address_wrapper( hModule, function );
-    } else {
-        return get_proc_address_wrapper( hModule, bullshit_name );
-    }
+    return get_proc_address_wrapper( hModule, function );
 }
